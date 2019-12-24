40601c2a154f12e26743e18a91792cfcd360ed71
Natalya Nechaeva <nechaevanatascha@yandex.ru>
2019-12-24 18:26:53 +0400

    Taks 1 module 3 done

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,10 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let userRes = data.filter(user => user.age <= age);
+  let result = '';
+  userRes.forEach(function addToString(element) {
+    result = `${result + element.name}, ${element.balance}\n`;
+  });
+  return result.slice(0, result.length - 1);
 }
