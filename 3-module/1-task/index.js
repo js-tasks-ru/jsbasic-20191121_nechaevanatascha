5287cfcea5f031d5861afb5d0aa9347bc9bc8582
Natalya Nechaeva <nechaevanatascha@yandex.ru>
2019-12-24 18:23:09 +0400

    Revert "Task 1 module 3 done"
    This reverts commit 3cfc6f5c461b0937b970e3d3e414684ddabdee3d.

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,11 +5,4 @@
  * @returns {string}
  */
 function showSalary(data, age) {
-  let userRes = data.filter(user => user.age <= age);
-  let result = '';
-  userRes.forEach(function addToString(element) {
-    //result = result + element.name + ', ' + element.balance + '\n';
-    result = `${result + element.name}, ${element.balance}\n`;
-  });
-  return result.slice(0, result.length - 1);
 }
